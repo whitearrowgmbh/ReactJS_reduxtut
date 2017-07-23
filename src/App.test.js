1d8b781a4fc30161ab49b781e83de886af02f5d0
Juergen Pichler <shop@whitearrow.de>
2017-07-23 12:41:32 +0200

    Enzyme ergänzt

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
+/* global it, expect */ // für Linting
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {shallow} from 'enzyme';
+
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+
+    const component = shallow(<App/>);
+    expect(component.exists())
+        .toEqual(true);
 });
